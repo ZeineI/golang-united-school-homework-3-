881d8fca6054db6cad4e0cd49e73a8fe03651fd8
Zeine <zeineshka2003@gmail.com>
2022-05-07 00:32:01 +0600

    first try

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,10 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	slc := make([]int64, len(input))
+	copy(slc, input)
+	for i, j := 0, len(slc)-1; i < j; i, j = i+1, j-1 {
+		slc[i], slc[j] = slc[j], slc[i]
+	}
+	return slc
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,55 @@
 package homework
 
+import (
+	"sort"
+)
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+	var res []string
+	keyArr := keyArr(input)
+	sort.Ints(keyArr)
+	for _, v := range keyArr {
+		res = append(res, input[v])
+	}
+	return res
 }
+
+func keyArr(input map[int]string) []int {
+	var res []int
+	for i := range input {
+		res = append(res, i)
+	}
+	return res
+}
+
+// func main() {
+// 	input := map[int]string{
+// 		0:  "M",
+// 		1:  "A",
+// 		2:  "Y",
+// 		3:  " ",
+// 		4:  "T",
+// 		5:  "H",
+// 		6:  "E",
+// 		7:  " ",
+// 		8:  "F",
+// 		9:  "O",
+// 		10: "R",
+// 		11: "C",
+// 		12: "E",
+// 		13: " ",
+// 		14: "B",
+// 		15: "E",
+// 		16: " ",
+// 		17: "W",
+// 		18: "I",
+// 		19: "T",
+// 		20: "H",
+// 		21: " ",
+// 		22: "Y",
+// 		23: "O",
+// 		24: "U",
+// 	}
+
+// 	fmt.Println(sortMapValues(input))
+// }
